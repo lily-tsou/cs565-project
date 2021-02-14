e75fb3d6509b2e9472d085a0b6bc7049e6cf5c90
michael <michael.r808@gmail.com>
2021-02-14 15:59:34 -0800

    Update list aside

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -12,6 +12,8 @@ import React from 'react';
 import '../styles/App.css';
 
 const url = 'http://localhost';
+const noteImg = 'https://icons.iconarchive.com/icons/johanchalibert/mac-osx-yosemite/1024/notes-icon.png';
+const bootstrap = 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css';
 
 export default class App extends React.Component {
 
@@ -29,19 +31,12 @@ export default class App extends React.Component {
         this.setState({ isLoading: true });
         fetch(url + '/list')
         .then(res => {
-            if(res.status >= 300) {
-                throw new Error(res.statusText);
-            }
+            if(res.status >= 300) { throw new Error(res.statusText); }
             return res.json();
         })
-        .then(list => {
-            this.setState({ list, isLoading: false });
-        })
+        .then(list => { this.setState({ list, isLoading: false }); })
         .catch(err => { 
-            this.setState({
-                err,
-                isLoading: false
-            });
+            this.setState({ err, isLoading: false});
             console.log(err); 
         });
     }
@@ -61,17 +56,24 @@ export default class App extends React.Component {
         
         return (
             <main className="container">
-                <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css"/>
+                <link rel="stylesheet" type="text/css" href={bootstrap}/>
                 <header>
                     <h1>NoteQuest</h1>
                 </header>
                 <section className="grid-container">
-                    <nav className="grid-item grid-item1">nav bar goes here</nav>
+                    <nav className="grid-item grid-item1">
+                        <ul className="navbar">
+                            <li className="navitem"><a href="#">New</a></li>
+                            <li className="navitem"><a href="#">Sort</a></li>
+                            <li className="navitem"><a href="#">Dark Mode</a></li>
+                            <li className="navitem"><a href="#">Contact</a></li>
+                        </ul>
+                    </nav>
                     <aside className="grid-item grid-item2">
-                        <ul>
+                        <ul className="notelist">
                             {list.map(item => {
-                                return <li key={item._id} id={item._id} >
-                                        { item.data }
+                                return <li className="noteitem" key={item._id} id={item._id} >
+                                        <a href="/"><img src={noteImg} width='50' height='50' /> { item.data.substr(0,5) } </a>
                                     </li>
                             })}
                         </ul>
@@ -79,7 +81,7 @@ export default class App extends React.Component {
                     <section className="grid-item grid-item3">
                         note textbox goes here
                     </section>
-                    <footer className="grid-item grid-item4">footer goes here</footer>
+                    <footer className="grid-item grid-item4">@Copyright: The NoteQuest Team </footer>
                 </section>
             </main>
         )
