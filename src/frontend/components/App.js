8e78823b8fff5d0dec232e94280b79dbd52197b4
lily-tsou <tsou@pdx.edu>
2021-02-17 18:05:08 -0800

    Worked on sidebar prototype

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -6,6 +6,7 @@
 
 import React from 'react';
 import '../styles/App.css';
+import NotePreview from './NotePreview';
 
 const url = 'http://localhost';
 const noteImg = 'https://icons.iconarchive.com/icons/johanchalibert/mac-osx-yosemite/1024/notes-icon.png';
@@ -131,12 +132,16 @@ export default class App extends React.Component {
                     </nav>
                     <aside className="grid-item grid-item2">
                         <ul className="notelist">
+
+                            {/* TODO: Make the title and body the actual title and body (todo) of note
+                            Associate the link with the entire NotePreview  */}
                             {list.map(item => {
                                 return <li className="noteitem" key={item._id} id={item._id} >
-                                        <a onClick={() => this.handleListSelect(item._id, item.data)} href="#"><img className="noteicon" src={noteImg}/>
-                                        { item.data.substr(0,6) } 
-                                        </a>
-                                    </li>
+                                    <a onClick={() => this.handleListSelect(item._id, item.data)} href="#"> 
+                                    { <NotePreview title = {item.data}
+                                      body = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '/>} 
+                                </a>
+                                </li>
                             })}
                         </ul>
                     </aside>
