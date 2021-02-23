16233f88d2924ebee397d81f0062cb29d9655c34
michael <michael.r808@gmail.com>
2021-02-23 14:05:41 -0800

    debug cors

--- src/backend/server.js
+++ src/backend/server.js
@@ -13,18 +13,17 @@ const routes = require('./routes');
 let startServer = () => {
     const app = express();
 
-    app.use(express.static('public'));
     app.use((req, res, next) => {
         res.setHeader("Access-Control-Allow-Origin", "*");
         res.header(
             "Access-Control-Allow-Headers",
             "Origin, X-Requested-With, Content-Type, Accept"
-          );
+        );
         next();
     });
+    app.use(express.static('public'));
     app.use(routes);
  
-
     const port = process.env.PORT || 80;
     const rc = app.listen(port, () => console.log(`Server listening on port: ${port}`));
     return rc.listening;  // true if server is listening
