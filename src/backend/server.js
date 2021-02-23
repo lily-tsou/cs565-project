f941e37e06f9a3d3f4fc4ae165a68f81b1693e8f
michael <michael.r808@gmail.com>
2021-02-23 13:53:22 -0800

    update

--- src/backend/server.js
+++ src/backend/server.js
@@ -16,6 +16,11 @@ let startServer = () => {
     app.use(express.static('public'));
     app.use((req, res, next) => {
         res.setHeader("Access-Control-Allow-Origin", "*");
+        res.header(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept"
+          );
+        next();
     });
     app.use(routes);
  
