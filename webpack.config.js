6ab6a72835913866b78bee2b7f3626516e19730c
michael <michael.r808@gmail.com>
2021-03-03 19:01:50 -0800

    Debug GKE

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 // const backend = {
 //   entry: './src/backend/server.js',
@@ -46,12 +46,12 @@ const frontend = {
         loader: 'file-loader'
       }
     ]
-  }//,
-  // plugins: [
-  //   new HtmlWebpackPlugin({
-  //     template: "./src/frontend/index.html"
-  //   })
-  // ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/frontend/index.html"
+    })
+  ]
 };
 
 module.exports = frontend;
